ab7fec37e2f1555c34bcebd2611beca6daad8a22
jsfernando <jsfernando@gmail.com>
2021-03-10 09:29:02 -0300

    Tab option in ion-action-sheet and option.page delete

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -20,10 +20,6 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
-        path: 'options',
-        loadChildren: () => import('../options/options.module').then( m => m.OptionsPageModule)
-      },
-      {
         path: '',
         redirectTo: '/tabs/pain',
         pathMatch: 'full'

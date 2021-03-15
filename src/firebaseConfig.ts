408a3170a1186842290694295871883ecc734f25
fernandojsilvasenac <34111495+fernandojsilvasenac@users.noreply.github.com>
2021-03-15 07:55:48 -0300

    Delete firebaseConfig.ts
    delete firebaseConfig.ts

--- src/firebaseConfig.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyA-q6LHnfs3EFdsNLfbQ3zkSRebHwU1Wrg",
-  authDomain: "applibrassenac.firebaseapp.com",
-  projectId: "applibrassenac",
-  storageBucket: "applibrassenac.appspot.com",
-  messagingSenderId: "560231547514",
-  appId: "1:560231547514:web:2a12a4abd5c78969f631c3"
-}
